806fdb78381851cf103a8444796fd149abdcc01f
Edwin Caro Soto <v-edcaro@microsoft.com>
2020-01-06 16:18:46 -0600

    shellscript.sh v3

---
+++ shellscript1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Enter the source directory to make sure the script runs where the user expects+
+apt-get update
+apt-get install python-pip
+pip install virtualenv
+pip install uwsgi
+uwsgi --version
+cd /home/site/wwwroot
+# source antenv/bin/activate
+echo Im here
+export PORT=8000
+pserver deployment.ini
